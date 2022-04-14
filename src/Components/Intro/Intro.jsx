25802438bb586a60a8234216f5b3e232b6268e3d
sanaul01 <mdsanaulislam154@gmail.com>
2022-04-14 23:38:35 +0600

    services added

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -4,8 +4,12 @@ import Linkedin from '../../images/linkedin.png';
 import Instagram from '../../images/instagram.png';
 import Vector1 from '../../images/Vector1.png';
 import Vector2 from '../../images/Vector2.png';
+import crown from '../../images/crown.png';
+import thumbup from '../../images/thumbup.png';
+import glassesimoji from '../../images/glassesimoji.png'
 import Boy from '../../images/boy.png';
 import './Intro.css'
+import FloatingDiv from '../FloatingDiv/FloatingDiv';
 
 const Intro = () => {
     return (
@@ -25,11 +29,28 @@ const Intro = () => {
             </div>
 
             <div className="i-right">
-                
-                    <img src={Vector1} alt="" />
-                    <img src={Vector2} alt="" />
-                    <img src={Boy} alt="" />
-               
+
+                <img src={Vector1} alt="" />
+                <img src={Vector2} alt="" />
+                <img src={Boy} alt="" />
+                <img src={glassesimoji} alt="" />
+                <div style={{ top: '-4%', left: '68%' }}>
+                    <FloatingDiv image={crown} txt1='Web' txt2='Developer' />
+                </div>
+                <div style={{ top: '18rem', left: '0rem' }}>
+                    <FloatingDiv image={thumbup} txt1='Best Design' txt2='Award' />
+                </div>
+                {/* blur divs */}
+                <div className="blur" style={{background: "rgb(238 210 255)"}}>    
+                </div>
+
+                <div className="blur" style={{
+                    background: '#c1f5ff',
+                    top: '17rem', 
+                    width: '21rem',
+                    heigh: '11rem',
+                    left: '-9rem'
+                }}></div>
             </div>
         </div>
     );
---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Services.css';
+import heartemoji from '../../images/heartemoji.png';
+import glasses from '../../images/glasses.png';
+import humbler from '../../images/humble.png';
+
+const Services = () => {
+    return (
+        <div className="services">
+            {/* left side */}
+            <div className="awesome">
+                <span>My Awesome</span>
+                <span>Services</span>
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
+                    <br />
+                     aut sequi illo voluptatem tenetur quam?</span>
+
+            </div>
+            {/* right side */}
+            <div className="cards">
+
+            </div>
+        </div>
+    );
+};
+
+export default Services;
