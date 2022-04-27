d6f61ae0e696bbcc695d42844dde22166c9794b3
sanaul01 <mdsanaulislam154@gmail.com>
2022-04-27 17:28:25 +0600

    added dark mode and others

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import { useContext } from 'react';
 import './App.css';
 import Contact from './Components/Contact/Contact';
 import Experience from './Components/Experience/Experience';
@@ -9,10 +10,20 @@ import Portfolio from './Components/Portfolio/Portfolio';
 import Services from './Components/Services/Services';
 import Testimonials from './Components/Testimonials/Testimonials';
 import Works from './Components/Works/Works';
+import { themeContext } from './Context';
 
 function App() {
+
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+
   return (
-    <div className="App">
+    <div className="App"
+      style={{ background: darkMode? 'black': '',
+              color: darkMode? 'white': '',
+    }}
+    >
       <Navbar />
       <Intro />
       <Services />
