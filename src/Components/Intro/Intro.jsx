a01f8131eb40a8412c4dcd6e906d72f8c1753106
sanaul01 <mdsanaulislam154@gmail.com>
2022-04-28 16:32:30 +0600

    added animation

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -11,8 +11,11 @@ import Boy from '../../images/boy.png';
 import './Intro.css'
 import FloatingDiv from '../FloatingDiv/FloatingDiv';
 import { themeContext } from '../../Context';
+import {motion} from 'framer-motion'
 
 const Intro = () => {
+
+    const transition = {duration: 2, type: 'spring'}
 
     const theme = useContext(themeContext);
     const darkMode = theme.state.darkMode;
@@ -39,13 +42,28 @@ const Intro = () => {
                 <img src={Vector1} alt="" />
                 <img src={Vector2} alt="" />
                 <img src={Boy} alt="" />
-                <img src={glassesimoji} alt="" />
-                <div style={{ top: '-4%', left: '68%' }}>
+                <motion.img
+                initial={{left: '-36%'}}
+                whileInView={{left: '-24%'}}
+                transition={{transition}}
+
+                src={glassesimoji} alt="" />
+                <motion.div 
+                initial={{top: '-4%',left: '74%'}}
+                whileInView={{left: '68%'}}
+                transition={{transition}}
+
+                style={{ top: '-4%', left: '68%' }}>
                     <FloatingDiv image={crown} txt1='Web' txt2='Developer' />
-                </div>
-                <div style={{ top: '18rem', left: '0rem' }}>
+                </motion.div>
+                <motion.div 
+                initial={{top: '9rem',left: '18rem'}}
+                whileInView={{left: '0rem'}}
+                transition={{transition}}
+
+                style={{ top: '18rem', left: '0rem' }}>
                     <FloatingDiv image={thumbup} txt1='Best Design' txt2='Award' />
-                </div>
+                </motion.div>
                 {/* blur divs */}
                 <div className="blur" style={{ background: "rgb(238 210 255)" }}>
                 </div>
